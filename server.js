ab9b6db98bc98e0975b2434adec405b68fda35dd
william huang <will.h86@gmail.com>
2015-02-21 00:16:51 -0800

    8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express     = require('express');
 var app         = express();
-var port        = process.env.PORT || 3000;
+var port        = process.env.PORT || 8080;
 var path        = __dirname + '/assets/';
 var bodyparser  = require('body-parser');
 var compression = require('compression');
