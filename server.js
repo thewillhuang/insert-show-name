5d021872fbbacbb313bc5c35ac30f54e7498f7ce
william huang <will.h86@gmail.com>
2015-03-19 22:21:31 -0700

    setup ghost blogging

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var path        = __dirname + '/assets/';
 var bodyparser  = require('body-parser');
 var compression = require('compression');
 var oneDay = 86400000;
+var ghost = require('ghost');
 // var mongoose    = require('mongoose');
 // var uriUtil     = require('mongodb-uri');
 // var options     = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
@@ -19,6 +20,12 @@ app.enable('etag');
 app.use(compression());
 app.use(bodyparser.json());
 app.use(express.static(path));
+
+ghost().then(function (ghostServer) {
+    app.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+
+    ghostServer.start(app);
+});
 
 app.listen(port, function() {
   console.log('Server started on port: %d', port);
